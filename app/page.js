c6399b602c183df1b2331f41415494bbf3bbea9c
Goloviychuk Vadim <vadim.goloviychuk@gmail.com>
2024-11-10 19:02:26 +0200

    the header was added

--- app/page.js
+++ app/page.js
@@ -1,25 +1,12 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { Header } from "@/components/Header";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
+        <Header />
         <div className={styles.ctas}>
           <a
             className={styles.primary}
---
+++ components/Header.jsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import styles from "@/app/page.module.css";
+
+export const Header = () => {
+  return (
+    [
+      <Image
+        className={styles.logo}
+        src="/next.svg"
+        alt="Next.js logo"
+        width={180}
+        height={38}
+        priority
+      />,
+      <ol>
+        <li>
+          Get started by editing <code>app/page.js</code>.
+        </li>
+        <li>Save and see your changes instantly.</li>
+        <li><Link href="/about">About Page</Link></li>
+      </ol>
+    ]
+  )
+};
